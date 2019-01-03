7654c629beb1a45df87e4c3c34bef1af4e5a2e44
Harry Nightingall <nightingallh@gmail.com>
2019-01-03 13:50:48 +0000

    basic-resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_01_02_215159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "score"
+    t.string "restaurant"
+    t.integer "price"
+    t.string "cuisine"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
