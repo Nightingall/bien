f208c9a5025b3226804867dcf3dd0b2fd60ad616
Harry Nightingall <nightingallh@gmail.com>
2019-01-12 21:05:24 +0000

    bookmark feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'reviews#index'
   resources  :reviews do
     resources :comments
+    resource :bookmark
   end
   resource :session
   resources :users
